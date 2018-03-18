585160050986e40cc92f0099c3e3d925f9c043a4
Roman <sektor_wins@mail.ru>
2018-03-19 02:19:23 +0500

    Для folder_processing добавлен параметр - comments_end, указывающий на последнюю строку комментариев в исходном IAGA-2002 файле

--- Utils/folder_processing.py
+++ Utils/folder_processing.py
@@ -5,7 +5,7 @@ from .clear_csv_maker import make_csv_from_original_data
 from .clear_csv_maker import replace_spaces_with_semicolon
 
 
-def folder_processing(original_path_or_dir: str, out_path_or_dir: str) -> None:
+def folder_processing(original_path_or_dir: str, out_path_or_dir: str, comments_end = 24) -> None:
     """
         Функция для трансформации наборов исходных IAGA-2002 файлов, хранящихся в 
         :original_path_or_dir директории, и получении выходных csv-файлов в
@@ -17,7 +17,7 @@ def folder_processing(original_path_or_dir: str, out_path_or_dir: str) -> None:
         if file.endswith(".min"):
             out_csv = out_path_or_dir + file + ".csv"
             print (f"Processing --- {file}")
-            make_csv_from_original_data(original_path_or_dir + file, out_csv, comments_end=23)
+            make_csv_from_original_data(original_path_or_dir + file, out_csv, comments_end=comments_end)
             replace_spaces_with_semicolon(out_csv)
             counter += 1
-    print (f"Processing is over.\nThere were processed {counter} files with {(time.time() - start_time).round(3)} seconds.")
+    print (f"Processing is over.\nThere were processed {counter} files with {(time.time() - start_time)} seconds.")
